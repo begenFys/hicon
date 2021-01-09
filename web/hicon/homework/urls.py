0d440655d7a1464a04ab05bc703264d1381293c6
Nikita Perminov <vip.nikita.2004@list.ru>
2021-01-09 22:38:33 +0500

    add method tm

--- web/hicon/homework/urls.py
+++ web/hicon/homework/urls.py
@@ -3,5 +3,9 @@ from .views import *
 
 
 urlpatterns = [
-    path('', homework, name='homework')
+    path('', homework, name='homework'),
+    path('pomodoro/', pomodoro, name='pomodoro'),
+    path('matrix/', matrix, name='matrix'),
+    path('frog/', frog, name='frog'),
+    path('salami/', salami, name='salami')
 ]
--- web/hicon/homework/views.py
+++ web/hicon/homework/views.py
@@ -8,3 +8,35 @@ def homework(request):
     return redirect('log')
   else:
     return render(request, 'homework/homework.html')
+
+def pomodoro(request):
+  try:
+    person = User.objects.get(id=request.user.id)
+  except:
+    return redirect('log')
+  else:
+    return render(request, 'homework/pomodoro.html')
+
+def matrix(request):
+  try:
+    person = User.objects.get(id=request.user.id)
+  except:
+    return redirect('log')
+  else:
+    return render(request, 'homework/matrix.html')
+
+def frog(request):
+  try:
+    person = User.objects.get(id=request.user.id)
+  except:
+    return redirect('log')
+  else:
+    return render(request, 'homework/frog.html')
+
+def salami(request):
+  try:
+    person = User.objects.get(id=request.user.id)
+  except:
+    return redirect('log')
+  else:
+    return render(request, 'homework/salami.html')
